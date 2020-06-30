212b9da9193ef146a6968bd6683c2504427ee03d
Andrei Voicu <dreivoicu@gmail.com>
2020-06-30 19:36:07 +0300

    modal in auxiliary route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ModalComponent } from './modal/modal.component';
+import { LoginComponent } from './login/login.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModalComponent,
+    LoginComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/modal/modal.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss']
+})
+export class ModalComponent implements OnInit {
+  @Output()
+  modalClose: EventEmitter<any> = new EventEmitter<any>();
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  closeModal($event: any): void {
+    this.router.navigate([{outlets: {modal: null}}]);
+    this.modalClose.next($event);
+  }
+}
